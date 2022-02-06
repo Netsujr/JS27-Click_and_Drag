fa9f58d7dcad5cae6f0c45119ff80a8190d5d478
Renato Delboni <renatonato_jr@hotmail.com>
2022-02-06 14:41:46 +0900

    added all mouse eventListeners

---
+++ app.js
@@ -0,0 +1,24 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', () => {
+
+
+});
+
+slider.addEventListener('mousemove', () => {
+
+
+});
+
+slider.addEventListener('mouseup', () => {
+
+
+});
+
+slider.addEventListener('mouseleave', () => {
+
+
+});
